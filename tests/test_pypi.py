4d3623b961f3a7fa79860fb5b9b224904a342915
Guillaume Binet <gbin@gootz.net>
2015-05-31 12:37:39 -0700

    err is generating too much noise to be tested on searchpy

--- tests/test_pypi.py
+++ tests/test_pypi.py
@@ -4,4 +4,4 @@ from errbot.backends.test import FullStackTest, pushMessage, popMessage
 class TestCommands(FullStackTest):
 
     def test_searchpy(self):
-        self.assertCommand('!searchpy err', 'err is a plugin based XMPP chatbot designed to be easily deployable, extensible and maintainable.')
+        self.assertCommand('!searchpy Werkzeug', 'The Swiss Army knife of Python web development')
