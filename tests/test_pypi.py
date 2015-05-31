4c103960568070688697334fff92c9bb33661348
Guillaume Binet <gbin@gootz.net>
2015-05-31 11:47:13 -0700

    correct tests

--- tests/test_pypi.py
+++ tests/test_pypi.py
@@ -2,9 +2,6 @@ from errbot.backends.test import FullStackTest, pushMessage, popMessage
 
 
 class TestCommands(FullStackTest):
-    @classmethod
-    def setUpClass(cls, extra=None):
-        super(TestCommands, cls).setUpClass(__file__)
 
     def test_searchpy(self):
         self.assertCommand('!searchpy err', 'err is a plugin based XMPP chatbot designed to be easily deployable, extensible and maintainable.')
